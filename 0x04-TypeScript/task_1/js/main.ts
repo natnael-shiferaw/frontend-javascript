f67b2b21ac2b99f5e58a2e2120880f5acbe21785
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2024-12-15 00:08:00 +0300

    build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,19 @@
+// create interface for Teacher
+interface Teacher {
+    readonly firstName: string,
+    readonly lastName: string,
+    fullTimeEmployee: boolean,
+    yearsOfExperience?: number,
+    location: string,
+    [key: string]: any
+}
+// example of the usage of Teacher interface
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
